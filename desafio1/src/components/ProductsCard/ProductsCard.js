6d780fe5a9a2198bcb269dc9c55eeae5c29ddf98
Biancasleonardo <biancasleonardo@gmail.com>
2021-07-12 16:55:44 -0300

    create layout ProductsCard

--- desafio1/src/components/ProductsCard/ProductsCard.js
+++ desafio1/src/components/ProductsCard/ProductsCard.js
@@ -1,10 +1,31 @@
+import { CardContent, Typography } from '@material-ui/core';
 import React from 'react';
+import { CardProduct, ContainerMeta, Score, Visits } from './style';
 
 const ProductsCard = () => {
     return (
-        <div>
-            ProductsCard
-        </div>
+        <CardProduct variant="outlined">
+            <CardContent>
+                <Typography variant="body1" component="h2">
+                    Nome do Produto
+                </Typography>
+                <Typography variant="body2">
+                    Composição: Orgânico
+                </Typography>
+                <ContainerMeta>
+                    <Score>
+                        <Typography variant="body2" component="span">
+                            Score: 35.0000
+                        </Typography>
+                    </Score>
+                    <Visits>
+                        <Typography variant="body2" component="span">
+                            Visualizações: 35
+                        </Typography>
+                    </Visits>
+                </ContainerMeta>
+            </CardContent>
+        </CardProduct>
     );
 };
 
---
+++ desafio1/src/components/ProductsCard/style.js
@@ -0,0 +1,20 @@
+import { Card } from "@material-ui/core";
+import styled from "styled-components";
+
+export const CardProduct = styled(Card)`
+    width: 250px;
+`
+
+export const ContainerMeta = styled.div`
+    display: flex;
+    justify-content: space-between;
+`
+
+export const Score = styled.div`
+    display: flex;
+    align-items: center;
+`
+export const Visits = styled.div`
+    display: flex;
+    align-items: center;
+`
