b71b6b2b5b925dcd35aa9637586395b479145896
Biancasleonardo <biancasleonardo@gmail.com>
2021-07-12 18:13:24 -0300

    search field working

--- desafio1/src/components/Search/Search.js
+++ desafio1/src/components/Search/Search.js
@@ -1,20 +1,25 @@
 import { Button, TextField } from '@material-ui/core';
-import React from 'react';
+import React, { useState } from 'react';
 import { SectionSearch } from './style';
 
 const Search = ({getProducts}) => {
+    const [inputSearch, setInputSearch] = useState('')
+
+    const onChangeInput = (e) => {
+        setInputSearch(e.target.value)
+    }
     return (
         <SectionSearch>
             <TextField
                 id="outlined-basic"
                 label="O que você está procurando?"
                 variant="outlined"
-                // value={''}
-                // onChange={''}
+                value={inputSearch}
+                onChange={onChangeInput}
                 size='small'
                 fullWidth
             />
-            <Button variant='contained' color='primary'onClick={() => getProducts('celular')}>Buscar</Button>
+            <Button variant='contained' color='primary'onClick={() => getProducts(inputSearch)}>Buscar</Button>
         </SectionSearch>
     );
 };
