3ddbe873c1d81a17db40edc5d0a524324a09aadd
Biancasleonardo <biancasleonardo@gmail.com>
2021-07-12 22:38:13 -0300

    create main

--- desafio2/src/components/Main/Main.js
+++ desafio2/src/components/Main/Main.js
@@ -1,18 +1,29 @@
 import React from 'react';
 import Image1 from '../../assets/Bitmap.svg'
-import { MainContainer } from './style';
+import Image2 from '../../assets/measuring-large.svg'
+import { ContainerDescription, ContainerImage, ContainerMain, ContainerMeasuring, ContainerTapes, MainContainer } from './style';
 
 const Main = () => {
     return (
-        <MainContainer>
-            <div>
-                <h2>Measuring everything</h2>
-            </div>
-            <div>
-                <img src={Image1} />
-            </div>
-
-        </MainContainer>
+        <ContainerMain>
+            <ContainerMeasuring>
+                <div>
+                    <h2>Measuring everything</h2>
+                </div>
+                <div>
+                    <img src={Image1} />
+                </div>
+            </ContainerMeasuring>
+            <ContainerTapes>
+                <ContainerDescription>
+                    <h3>We are Leader in Measure Tapes</h3>
+                    <p>There are 5x the circumference of planet earth in metric tapes.</p>
+                </ContainerDescription>
+                <ContainerImage>
+                    <img src={Image2} />
+                </ContainerImage>
+            </ContainerTapes>
+        </ContainerMain>
     );
 };
 
--- desafio2/src/components/Main/style.js
+++ desafio2/src/components/Main/style.js
@@ -1,10 +1,17 @@
 import styled from "styled-components";
 
-export const MainContainer = styled.main`
+export const ContainerMain = styled.main`
+    display: flex;
+    flex-direction: column;
+    font-family: 'Roboto Condensed';
+
+`
+
+export const ContainerMeasuring = styled.section`
     display: flex;
     align-items: center;
     justify-content: center;
-    height: 100vh;
+    height: 1024px;
     padding: 0 60px;
 
     background-color: #E9E9E9;
@@ -21,7 +28,6 @@ export const MainContainer = styled.main`
         text-align: end;
 
         h2{
-        font-family: 'Roboto Condensed';
         font-style: normal;
         font-weight: bold;
         font-size: 82.2px;
@@ -29,4 +35,45 @@ export const MainContainer = styled.main`
     }
 
    
+`
+
+export const ContainerTapes = styled.section`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: relative;
+    height: 1024px;
+
+    color: #FFFFFF;
+    background-color: #506FA9;
+
+    img{
+        width: 1038px;
+        height: 689px;
+        mix-blend-mode: multiply;
+    }
+
+    h3{
+        font-style: normal;
+        font-weight: bold;
+        font-size: 69.2px;
+    }
+
+    p{
+        font-style: normal;
+        font-weight: 300;
+        font-size: 48.2px;
+        margin: 0;
+    }
+`
+
+export const ContainerImage = styled.div`
+    width: 50%;
+`
+
+export const ContainerDescription = styled.div`
+    position: absolute;
+    width: 40%;
+    bottom: 100px;
+    left: 100px;
 `
